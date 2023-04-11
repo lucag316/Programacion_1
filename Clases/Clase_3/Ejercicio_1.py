fff9a9812be68531f89d4d765d4a5110d9741662
lucag316 <lucafranco316@gmail.com>
2023-04-11 15:14:32 -0300

    Update

--- Clases/Clase_3/Ejercicio_1.py
+++ Clases/Clase_3/Ejercicio_1.py
@@ -11,19 +11,31 @@
 # Se debe informar los datos de la compra procesados para poder iniciar el control de
 # stock.
 
+
 respuesta = "si"
 while respuesta == "si":
     tipo = input("Ingrese el tipo de producto [barbijo], [jabon] o [alcohol]: ")
     while tipo != "barbijo" and tipo != "jabon"  and tipo != "alcohol":
         tipo = input("ERROR, reingrese el tipo de producto [barbijo], [jabon] o [alcohol]: ")
-
-    precio = float(input("Ingrese el precio del producto: "))
-    while precio < 0:
-        precio = float(input("ERROR, reingrese el precio del producto: "))
-
-    cantidad_unidades = int(input("Ingrese la cantidad de unidades: "))
-    while cantidad_unidades < 0:
-        cantidad_unidades = int(input("ERROR, reingrese la cantidad de unidades: "))
+    while True:
+        try: 
+            precio = float(input("Ingrese el precio del producto: ")) 
+            if precio < 0:
+                precio = float(input("ERROR, reingrese el precio del producto: "))
+            else:
+                break
+        except ValueError:
+            print("ERROR, solo puede ingresar numeros")
+            
+    while True:
+        try:
+            cantidad_unidades = int(input("Ingrese la cantidad de unidades: "))
+            if cantidad_unidades < 0:
+                cantidad_unidades = int(input("ERROR, reingrese la cantidad de unidades: "))
+            else:
+                break
+        except ValueError:
+            print("ERROR, solo puede ingresar numeros")
 
     marca = input("Ingrese la marca del producto: ")
     while marca == "":
@@ -32,9 +44,37 @@ while respuesta == "si":
     fabricante = input("Ingrese el fabricante del producto: ")
     while fabricante == "":
         fabricante = input("ERROR, reingrese el fabricante del producto: ")
-
+    print("\n=========================================================================================")
     print("Tipo: {0} \nPrecio: {1} \nCantidad de unidades {2} \nMarca: {3} \nFabricante: {4}".format(tipo, precio, cantidad_unidades, marca, fabricante)) # NO SE DONDE QUIERE EL PRINT
-
+    print("=========================================================================================\n")
     respuesta = input("\n[si] para ingresar otro producto \n[otra tecla] para salir \n")
+
+
+
+# respuesta = "si"
+# while respuesta == "si":
+#     tipo = input("Ingrese el tipo de producto [barbijo], [jabon] o [alcohol]: ")
+#     while tipo != "barbijo" and tipo != "jabon"  and tipo != "alcohol":
+#         tipo = input("ERROR, reingrese el tipo de producto [barbijo], [jabon] o [alcohol]: ")
+        
+#     precio = float(input("Ingrese el precio del producto: ")) 
+#     while precio < 0:
+#         precio = float(input("ERROR, reingrese el precio del producto: "))
+
+#     cantidad_unidades = int(input("Ingrese la cantidad de unidades: "))
+#     while cantidad_unidades < 0:
+#         cantidad_unidades = int(input("ERROR, reingrese la cantidad de unidades: "))
+
+#     marca = input("Ingrese la marca del producto: ")
+#     while marca == "":
+#         marca = input("ERROR, reingrese la marca del producto: ")
+    
+#     fabricante = input("Ingrese el fabricante del producto: ")
+#     while fabricante == "":
+#         fabricante = input("ERROR, reingrese el fabricante del producto: ")
+
+#     print("Tipo: {0} \nPrecio: {1} \nCantidad de unidades {2} \nMarca: {3} \nFabricante: {4}".format(tipo, precio, cantidad_unidades, marca, fabricante)) # NO SE DONDE QUIERE EL PRINT
+
+#     respuesta = input("\n[si] para ingresar otro producto \n[otra tecla] para salir \n")
 
 
--- Clases/Clase_4/Ejercicio_1.py
+++ Clases/Clase_4/Ejercicio_1.py
@@ -6,26 +6,28 @@
 # mostrarlos por pantalla en forma secuencial y ordenada. Realizar el ejercicio sin usar
 # listas primero, y despues usando listas y comparar la composición del código.
 
+lista_auto = []
 
-respuesta = "si"
 
-while respuesta == "si":
+
+
+
+# respuesta = "si"
+
+# while respuesta == "si":
     
-    marca = input("\nIngrese la marca del auto: ")
-    while marca == "":
-        marca = input("ERROR, reingrese la marca del auto: ")
+#     marca = input("\nIngrese la marca del auto: ")
+#     while marca == "":
+#         marca = input("ERROR, reingrese la marca del auto: ")
     
-    año_modelo = int(input("Ingrese el año del modelo: "))
-    while año_modelo < 1900 or año_modelo > 2023:
-        año_modelo = int(input("ERROR, reingrese el año del modelo: "))
+#     año_modelo = int(input("Ingrese el año del modelo: "))
+#     while año_modelo < 1900 or año_modelo > 2023:
+#         año_modelo = int(input("ERROR, reingrese el año del modelo: "))
     
-    precio = float(input("Ingrese el precio del auto: "))
-    while precio < 0:
-        precio = float(input("ERROR, reingrese el precio del auto: "))
+#     precio = float(input("Ingrese el precio del auto: "))
+#     while precio < 0:
+#         precio = float(input("ERROR, reingrese el precio del auto: "))
     
-    respuesta = input("[si] para continuar \n[otra tecla] para salir\n")
-    print("Marca: {0} \nAño del modelo: {1} \nPrecio ${2}\n".format(marca, año_modelo, precio))
-    
-    
-    
-    
+#     respuesta = input("[si] para continuar \n[otra tecla] para salir\n")
+#     print("Marca: {0} \nAño del modelo: {1} \nPrecio ${2}\n".format(marca, año_modelo, precio))
+
