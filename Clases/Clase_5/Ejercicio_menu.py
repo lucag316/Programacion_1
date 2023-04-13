2415e3c037a1a518584ff04acfb8b4481d9e9c4e
lucag316 <lucafranco316@gmail.com>
2023-04-12 21:10:31 -0300

    Update

--- Clases/Clase_5/Ejercicio_menu.py
+++ Clases/Clase_5/Ejercicio_menu.py
@@ -1,4 +1,5 @@
-# Ejercicio con Menú de Opciones
+# ERJERCICIO CON MENU DE OPCIONES
+
 # Realizar un programa utilizando los conceptos de la clase del lunes:
 # Opciones del menú:
 # # 1 cargar una lista con 10 números
@@ -16,8 +17,7 @@
 # Si se vacía la lista con la opción 10 se deben bloquear nuevamente las opciones hasta que se
 # cargue de nuevo los números con la opción 1
 
-
-lista_numeros = [5, 0, 50, -60, 7, 10, -99, 85, 74, 0]
+lista_numeros = []
 
 acumulador_numeros = 0
 cantidad_numeros = 0
@@ -27,19 +27,26 @@ contador_positivos = 0
 contador_negativos = 0
 contador_ceros = 0
 
+for i in range(3):
+    numero_appendeado= int(input("Ingrese un numero a la lista: "))
+    lista_numeros.append(numero_appendeado)
+    
 menor = lista_numeros[0]
 mayor = lista_numeros[0]
 
-
-
 numero_ingresado = int(input("Ingrese un numero: "))
-# print(f"indices en los que aparece {indice}")
-# indice = lista_numeros.index(numero)
+
 for numero in lista_numeros:
+    
+    #cantidad = lista_numeros.count(numero_ingresado)
+    # if  cantidad > 1:
+    #     print("el numero si esta en la lista")
+    #     break
+    
     if numero_ingresado == numero:
-        print("el numero si esta en la lista")
+        print("el numero si esta en la lista")      #TIENE QUE APARECER 1 VEZ
         indice = lista_numeros.index(numero)
-        print(f"indices en los que aparece {indice}")
+        print(f"indices en los que aparece: {indice}") # TIENE QUE APARECER MAS DE 1 INDICE
 
 for numero in lista_numeros:
     if numero % 2 == 0:
@@ -65,8 +72,7 @@ for numero in lista_numeros:
         menor = numero
 
 promedio_numeros = acumulador_numeros / cantidad_numeros
-# # 7 pedir un número e informar todos los índices donde aparece
-# # 10 vaciar lista
+
 lista_vacia = lista_numeros.clear()
 print(acumulador_numeros)
 print(promedio_numeros)
@@ -74,7 +80,49 @@ print(mayor)
 print(menor)
 print(f"pares {contador_pares}    |   impares: {contador_impares}")
 print(f"positivos: {contador_positivos}  | negativos: {contador_negativos}    | ceros: {contador_ceros}")
-print(f"lista vacia: {lista}")
+print(f"lista vacia: {lista_vacia}")
+
+# EL PUNTO 7, LO DEL  INDICE ESTA MAL HECHO
+
+# while True:
+#     opciones = print("1- Carga una lista con 10 numeros \n2- Muestra el total de los numeros ingresados \n3- Muestra el promedio n\4- Muestra el numero mayor n\5- Muestra el numero menor n\6- Pide un numero y dice si esta en la lista \n7- Pide un numero e informa todos los índices donde aparece \n8- Informa cuantos numeros pares e impares hay en la lista \n9- Informa cuantos positivos, negativos y veros hay en la lista \n10- Vacia la lista \n11- Salir")
+    
+#     opcion = input("Elija una opcion: ")
+    
+#     if opcion == 1:
+#         pass
+#     elif opcion == 2:
+#         pass
+#     elif opcion == 3:
+#         pass
+#     elif opcion == 4:
+#         pass
+#     elif opcion == 5:
+#         pass
+#     elif opcion == 6:
+#         pass
+#     elif opcion == 7:
+#         pass
+#     elif opcion == 8:
+#         pass
+#     elif opcion == 9:
+#         pass
+#     elif opcion == 10:
+#         pass
+#     elif opcion == 11:
+#         break
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
