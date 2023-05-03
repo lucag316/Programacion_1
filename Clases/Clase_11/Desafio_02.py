eaf60d7c74cab2f20bde547b80df95cb142bf788
lucag316 <lucafranco316@gmail.com>
2023-05-03 19:19:48 -0300

    Update

--- Clases/Clase_11/Desafio_02.py
+++ Clases/Clase_11/Desafio_02.py
@@ -1,50 +1,8 @@
 # Desafío #02: (con biblioteca propia: stark_biblioteca)
-# En base a lo resuelto en el desafío #00, deberás mejorar la calidad de tus funciones. Es por ello que te proponemos lo siguiente:
-# IMPORTANTE: Para todas y cada una de las funciones creadas, documentarlas escribiendo que es lo que hacen, que son los parámetros que reciben (si es una lista, un string, etc y que contendrá) y que es lo que retorna la función!
-# Crear la función 'stark_normalizar_datos' la cual recibirá por parámetro la lista de héroes. La función deberá:
-# Recorrer la lista y convertir al tipo de dato correcto las keys (solo con las keys que representan datos numéricos)
-# Validar primero que el tipo de dato no sea del tipo al cual será casteado. Por ejemplo si una key debería ser entero (ejemplo edad) verificar antes que no se encuentre ya en ese tipo de dato.
-# Si al menos un dato fue modificado, la función deberá imprimir como mensaje ‘Datos normalizados’, caso contrario no imprimirá nada.
-# Validar que la lista de héroes no esté vacía para realizar sus acciones, caso contrario imprimirá el mensaje: “Error: Lista de héroes vacía”
 
-
-# Crear la función 'obtener_nombre' la cual recibirá por parámetro un diccionario el cual representara a un héroe y devolverá un string el cual contenga su nombre formateado de la siguiente manera: 
-# Nombre: Howard the Duck
-
-
-# Crear la función 'imprimir_dato' la cual recibirá por parámetro un string y deberá imprimirlo en la consola. La función no tendrá retorno.
-
-
-# Crear la función 'stark_imprimir_nombres_heroes' la cual recibirá por parámetro la lista de héroes y deberá imprimirla en la consola. Reutilizar las funciones hechas en los puntos 1.1 y 1.2. Validar que la lista no esté vacía para realizar sus acciones, caso contrario no hará nada y retornara -1.
-# Con este se resuelve el Ej 1 del desafío #00
-
-
-# Crear la función 'obtener_nombre_y_dato' la misma recibirá por parámetro un diccionario el cual representara a un héroe y una key (string) la cual representará el dato que se desea obtener. 
-
-
-# La función deberá devolver un string el cual contenga el nombre y dato (key) del héroe a imprimir. El dato puede ser 'altura', 'fuerza', 'peso' O CUALQUIER OTRO DATO.
-
-
-# El string resultante debe estar formateado de la siguiente manera: (suponiendo que la key es fuerza)
-# Nombre: Venom | fuerza: 500
-
-
-# Crear la función 'stark_imprimir_nombres_alturas' la cual recibirá por parámetro la lista de héroes, la cual deberá iterar e imprimir sus nombres y alturas USANDO la función creada en el punto 2. Validar que la lista de héroes no esté vacía para realizar sus acciones, caso contrario no hará nada y retornara -1.
-# Con este se resuelve el Ej 2 del desafío #00
 from data_stark import lista_personajes
 import os
-"""
-    "nombre": "Howard the Duck",
-    "identidad": "Howard (Last name unrevealed)",
-    "empresa": "Marvel Comics",
-    "altura": "79.349999999999994",
-    "peso": "18.449999999999999",
-    "genero": "M",
-    "color_ojos": "Brown",
-    "color_pelo": "Yellow",
-    "fuerza": "2",
-    "inteligencia": ""
-"""
+
 #---------------------------------------------------------------------0
 def stark_normalizar_datos(lista_heroes: list):
     """
@@ -84,7 +42,6 @@ def obtener_nombre(heroe: dict) -> str:
     nombre = heroe["nombre"]
     mensaje = f"Nombre: {nombre}"
     return mensaje
-#obtener_nombre(lista_personajes[0])
 
 #---------------------------------------------------------------------1.2
 def imprimir_dato(cadena: str):
@@ -115,8 +72,6 @@ def stark_imprimir_nombres_heroes(lista_heroes: list):
     else:
         return imprimir_dato("-1")         # aca dice: caso contrario no hará nada y retornara -1.(no se si esta bien porque enrealidad no retorna, imprime)
 
-#stark_imprimir_nombres_heroes(lista_personajes)
-
 #---------------------------------------------------------------------2
 def obtener_nombre_y_dato(heroe: dict,  key: str) -> str:
     """
@@ -132,7 +87,6 @@ def obtener_nombre_y_dato(heroe: dict,  key: str) -> str:
     dato = heroe[key]
     mensaje = f"{nombre} | {key}: {dato}"
     return mensaje
-#print(obtener_nombre_y_dato(lista_personajes[0], "altura"))
 
 #---------------------------------------------------------------------3
 def stark_imprimir_nombres_alturas(lista_heroes: list):
@@ -149,8 +103,6 @@ def stark_imprimir_nombres_alturas(lista_heroes: list):
             print(obtener_nombre_y_dato(heroe, "altura"))
     else:
         return print("-1")
-
-#stark_imprimir_nombres_alturas(lista_personajes)
 
 #---------------------------------------------------------------------4.1
 def calcular_max(lista_heroes: list, key: str) -> dict:
@@ -169,8 +121,6 @@ def calcular_max(lista_heroes: list, key: str) -> dict:
             max = heroe
     return max
 
-#print(calcular_max(lista_personajes, "altura"))
-
 #---------------------------------------------------------------------4.2
 def calcular_min(lista_heroes: list, key: str) -> dict:
     """
@@ -187,8 +137,6 @@ def calcular_min(lista_heroes: list, key: str) -> dict:
         if heroe[key] < min[key]:
             min = heroe
     return min
-
-#print(calcular_min(lista_personajes, "altura"))
 
 #---------------------------------------------------------------------4.3
 def calcular_max_min_dato(lista_heroes: list, tipo_calculo: str, key: str) -> dict:
@@ -209,8 +157,6 @@ def calcular_max_min_dato(lista_heroes: list, tipo_calculo: str, key: str) -> di
         
     return max_min
 
-#print(calcular_max_min_dato(lista_personajes, "min", "peso"))
-
 #---------------------------------------------------------------------4.4
 def stark_calcular_imprimir_heroe(lista_heroes: list, tipo_calculo: str, key: str):
     """
@@ -229,10 +175,8 @@ def stark_calcular_imprimir_heroe(lista_heroes: list, tipo_calculo: str, key: st
     else:
         return -1
 
-#stark_calcular_imprimir_heroe(lista_personajes, "max", "altura")
-
 #---------------------------------------------------------------------5.1
-def sumar_dato_heroe(lista_heroes: list, key: str) -> float: # en realidad puede devolver int o float
+def sumar_dato_heroe(lista_heroes: list, key: str) -> float: 
     """
     Brief: Suma los datos de los heroes segun la key que se pase por parametro, antes de calcular se valida que el  heroe sea dict y que no este vacio
     
@@ -242,6 +186,7 @@ def sumar_dato_heroe(lista_heroes: list, key: str) -> float: # en realidad puede
     
     Return: Devuelve la suma de todos los datos segun la key pasada por parametro
     """
+    # en realidad puede devolver int o float
     suma = 0
     
     for heroe in lista_heroes:
@@ -256,8 +201,6 @@ def sumar_dato_heroe(lista_heroes: list, key: str) -> float: # en realidad puede
         #     suma += float(heroe[key])
             
     return suma
-
-#print(sumar_dato_heroe(lista_personajes, "fuerza"))
 
 #---------------------------------------------------------------------5.2
 def dividir(dividendo: int, divisor: int) -> float:
@@ -277,8 +220,6 @@ def dividir(dividendo: int, divisor: int) -> float:
         
     return retorno
 
-#print(dividir(20, 5))
-
 #---------------------------------------------------------------------5.3
 def calcular_promedio(lista_heroes: list, key: str) -> float:
     """
@@ -297,8 +238,6 @@ def calcular_promedio(lista_heroes: list, key: str) -> float:
         
     return promedio
 
-#print(calcular_promedio(lista_personajes, "altura"))
-
 #---------------------------------------------------------------------5.4
 def stark_calcular_imprimir_promedio_altura(lista_heroes: list):
     """
@@ -315,8 +254,6 @@ def stark_calcular_imprimir_promedio_altura(lista_heroes: list):
         imprimir_dato(promedio)
     else:
         return print(-1)
-
-#stark_calcular_imprimir_promedio_altura(lista_personajes)
 
 #---------------------------------------------------------------------6.1
 def imprimir_menu():
@@ -387,11 +324,11 @@ def stark_marvel_app(lista_heroes: list):
     
     Return: No tiene retorno, imprime segun la opcion que se seleccione
     """
+    
     # agregue la funcion calcular_promedio() aunque no empieza con "stark"
     os.system("cls")
     while True:
         opcion = stark_menu_principal()
-        
         match(opcion):
             case 1:
                 stark_imprimir_nombres_heroes(lista_personajes)
